1e3b287cbc6dda31c455ba77afe626c6ed575476
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-12-21 17:50:25 +0000

    stack & queue

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Stack<string> stack = new Stack<string>();
+stack.Push("a");
+stack.Push("b");
+Console.WriteLine(stack.Peek());
+stack.Pop();
+Console.WriteLine(stack.Peek());
+
+Queue<int> queue = new Queue<int>();
+queue.Enqueue(1);
+queue.Enqueue(2);
+Console.WriteLine(queue.Peek());
+queue.Dequeue();
+Console.WriteLine(queue.Peek());
+
+
