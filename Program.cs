c694311a5448e608739c0b591a241dbf13bbe935
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-12-29 17:47:51 +0000

    using multiple Queues to process ticketing

--- Program.cs
+++ Program.cs
@@ -1,24 +1,67 @@
-﻿using D4_challenge;
+﻿//using D4_challenge;
 
-static Node CreateExampleTree()
+//static Node CreateExampleTree()
+//{
+//    Node root = new Node("1",
+//        new Node("2", new Node("5", new Node("9"), null, null), null, null),
+//        new Node("3", new Node("6", new Node("10"), null, null), new Node("7"), null),
+//        new Node("4", new Node("8"), null, null)
+//        );
+
+//    return root;
+//}
+
+
+//Node myTree = CreateExampleTree();
+
+//Console.WriteLine("----------------------------");
+//Console.WriteLine("Breadth-First Search BFS Algorithm in C#");
+
+//Console.WriteLine("----------------------------");
+
+//BFSTraversal.BFSTransverse(myTree);
+
+var cts = new CancellationTokenSource();
+
+var normalQ = new Queue<string>();
+var goldQ = new Queue<string>();
+var platinumQ = new Queue<string>();
+
+normalQ.Enqueue("Aaron");
+goldQ.Enqueue("Ben");
+platinumQ.Enqueue("Carson");
+goldQ.Enqueue("Dickson");
+platinumQ.Enqueue("Eason");
+normalQ.Enqueue("Fishy");
+
+while (!cts.IsCancellationRequested)
 {
-    Node root = new Node("1",
-        new Node("2", new Node("5", new Node("9"), null, null), null, null),
-        new Node("3", new Node("6", new Node("10"), null, null), new Node("7"), null),
-        new Node("4", new Node("8"), null, null)
-        );
+    await Task.Delay(1000);
 
-    return root;
+    if (platinumQ.Count>0)
+    {
+        var platinumItem = platinumQ.Dequeue();
+        Console.WriteLine($"Platinum: {platinumItem}");
+        continue;
+    }
+
+    if (goldQ.Count > 0)
+    {
+        var goldItem = goldQ.Dequeue();
+        Console.WriteLine($"Gold: {goldItem}");
+        continue;
+    }
+
+    if (normalQ.Count > 0)
+    {
+        var normalItem = normalQ.Dequeue();
+        Console.WriteLine($"Normal: {normalItem}");
+        continue;
+    }
+
 }
 
 
-Node myTree = CreateExampleTree();
-
-Console.WriteLine("----------------------------");
-Console.WriteLine("Breadth-First Search BFS Algorithm in C#");
-Console.WriteLine("----------------------------");
-
-BFSTraversal.BFSTransverse(myTree);
 
 
 
